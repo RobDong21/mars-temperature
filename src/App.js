60c840791dc09391b88dd27810b8cb7fb630175a
RobDong <rob@Robs-MacBook-Pro.local>
2020-10-12 17:45:17 +0800

    call temperature api and display it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 
-function App() {
+import { api } from './service/api'
+import { Temperature } from './components/temperature/temperature.component'
+
+const App = () => {
+  const [temperature, setTemperature] = useState(null)
+
+  useEffect(() => {
+    (async function () {
+      try {
+        const latestMarsTemperature = await api.getLatestMarsTemperature()
+        setTemperature(latestMarsTemperature)
+      } catch (e) {
+        console.error(e);
+      }
+    })()
+  }, [])
+
   return (
     <div>
-      Mars temperature is: XX
+      {
+        temperature && <Temperature temperature={temperature} />
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export { App }
