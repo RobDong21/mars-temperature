948edded11969923212be0244dd39b4ec01b00d0
RobDong <rob@Robs-MacBook-Pro.local>
2020-10-12 21:31:18 +0800

    update Temperature component with responsive fontsize

--- src/components/temperature/temperature.component.styles.js
+++ src/components/temperature/temperature.component.styles.js
@@ -1,15 +1,26 @@
 import styled from 'styled-components'
 
+import { BREAKPOINT } from '../../design/breakpoint.constant'
+import { FONTSIZE } from '../../design/fontsize.constant'
+
 const Label = styled.p`
-  font-size: 16px;
+  font-size: ${FONTSIZE.S};
   text-align: center;
   margin-bottom: 8px;
 `
 
 const Value = styled.p`
-  font-size: 56px;
+  font-size: ${FONTSIZE.M};
   text-align: center;
-  margin-bottom: 36px;
+  margin-bottom: 56px;
+
+  @media only screen and (min-width: ${BREAKPOINT.tablet}) {
+    font-size: ${FONTSIZE.L};
+  }
+
+  @media only screen and (min-width: ${BREAKPOINT.desktop}) {
+    font-size: ${FONTSIZE.XL};
+  }
 `
 
 export { Label, Value }
--- src/design/typography.js
+++ src/design/typography.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components'
 
+import { FONTSIZE } from './fontsize.constant'
+
 const Body = styled.p`
-  font-size: 16px;
+  font-size: ${FONTSIZE.S};
+  text-align: center;
   margin-bottom: 80px;
 `
 
